87234095e122eab433d75740739ff2dc5b93ed0e
caballo <imanol.l.p@hotmail.com>
2021-10-14 00:41:13 +0200

    Started adding delete buttons and did some bugfixing

--- src/Components/EducationItem.js
+++ src/Components/EducationItem.js
@@ -1,12 +1,49 @@
 import React from 'react';
+import trashIcon from '../trash-bin.png';
 
-function EducationItem(props) {
-	return (
-		<div className="mainContain">
-			<h3>{props.place}</h3>
-			<p>{props.period} | {props.course}</p>
-		</div>
-	);
+class EducationItem extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			style: {display: 'none'}
+		}
+
+		this.handleMouseEnter = this.handleMouseEnter.bind(this);
+		this.handleMouseLeave = this.handleMouseLeave.bind(this)
+	}
+
+	handleMouseEnter() {
+		this.setState({
+			style: {display: 'block'}
+		})
+	}
+
+	handleMouseLeave() {
+		this.setState({
+			style: {display: 'none'}
+		})
+	}
+
+	handleDelete() {
+		const key = this.props
+
+
+	}
+
+	render() {
+		return (
+			<div onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>
+				<div className="education__container">
+					<h3>{this.props.place}</h3>
+					<p>
+						{this.props.period} | {this.props.course}
+					</p>
+				</div>
+				<img style={this.state.style} className="trashIcon" src={trashIcon} alt="Trashcan Icon" onClick={this.handleDelete}/>
+			</div>
+		);
+	}
 }
 
 export default EducationItem;
